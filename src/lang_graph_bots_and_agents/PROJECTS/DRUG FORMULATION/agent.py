fed487f4e60829ab7afa4d28d29ff56691617773
malik09-teach <malikabbaskhan477@gmail.com>
2026-09-21 15:30:34 +0500

    record uptade

--- src/lang_graph_bots_and_agents/PROJECTS/DRUG FORMULATION/agent.py
+++ src/lang_graph_bots_and_agents/PROJECTS/DRUG FORMULATION/agent.py
@@ -19,7 +19,7 @@ os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGSMITH_PROJECT")
 
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 
-llm = ChatGroq(model="openai/gpt-oss-120b")
+llm = ChatOllama(model="medgemma:4b")
 # ==========================================
 # 1. GRAPH STATE
 # ==========================================
